0b102609ff6bcb22a560467fdff693aec5ecf77b
DanielL29 <danielnatsufire@gmail.com>
2022-07-20 23:25:22 -0300

    estrutura inicial

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+import chalk from 'chalk'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(process.env.PORT, () => 
+    console.log(chalk.bgBlue(`Server listening on port ${process.env.PORT}`))
+)
