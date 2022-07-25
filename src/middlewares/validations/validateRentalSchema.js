bfca96df30059e3c5c9ba3ddac5bce915f29ed45
DanielL29 <danielnatsufire@gmail.com>
2022-07-25 15:56:52 -0300

    fix: ajustando pra contabilizar os jogos alugados que forem null

--- src/middlewares/validations/validateRentalSchema.js
+++ src/middlewares/validations/validateRentalSchema.js
@@ -12,7 +12,7 @@ async function validateRentalSchema(req, res, next) {
 
     const { rows: customerFounded } = await connection.query('SELECT * FROM customers WHERE id = $1', [customerId])
     const { rows: gameFounded } = await connection.query('SELECT * FROM games WHERE id = $1', [gameId])
-    const { rows: rentals } = await connection.query('SELECT * FROM rentals WHERE "gameId" = $1', [gameId])
+    const { rows: rentals } = await connection.query('SELECT * FROM rentals WHERE "gameId" = $1 AND "returnDate" IS NULL', [gameId])
 
     if(customerFounded.length === 0 || gameFounded.length === 0) {
         return res.status(400).send('customer or game not exists')
